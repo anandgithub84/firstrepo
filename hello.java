a865135ed4de40ef55273b16c4f7e67fe1bfd886
anandgithub84 <91039662+anandgithub84@users.noreply.github.com>
2021-10-13 22:25:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
-	public static void main (string[] args) {
+	public static void main (String[] args) {
 	// Prints "Hello, World" in the terminal window.
- 	system.out.println("Hello, World");
+ 	System.out.println("Hello, World");
 	}
 }
